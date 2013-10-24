96cf21552fb011584ed3d20c003d500ef57a9b6c
cosmik2002 <2007.nicholas@gmail.com>
2013-10-24 18:00:09 +0400

    change

--- src/com/example/todolist/MainActivity.java
+++ src/com/example/todolist/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.todolist;
 
 import java.util.ArrayList;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -11,34 +14,21 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class MainActivity extends Activity implements NewItemFragment.OnNewItemAddedListener {
+	ArrayList<String> todoItems;
+	ArrayAdapter<String> aa;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ListView myListView = (ListView)findViewById(R.id.myListView);
-		final EditText myEditText = (EditText)findViewById(R.id.myEditText);
-		
-		final ArrayList<String> todoItems = new ArrayList<String>();
-		final ArrayAdapter<String> aa;
+        FragmentManager fm = getFragmentManager();
+        ToDoListFragment todoListFragment = (ToDoListFragment)fm.findFragmentById(R.id.TodolIstFragment);
+        
+		todoItems= new ArrayList<String>();
 		aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,todoItems);
-		myListView.setAdapter(aa);
-		myEditText.setOnKeyListener(new View.OnKeyListener() {
-					
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if(event.getAction()== KeyEvent.ACTION_DOWN)
-					if((keyCode == KeyEvent.KEYCODE_DPAD_CENTER) ||
-						(keyCode == KeyEvent.KEYCODE_ENTER)){
-					 todoItems.add(0,myEditText.getText().toString());
-					 aa.notifyDataSetChanged();
-					 myEditText.setText("");
-					 return true;
-					}
-					
-				return false;
-			}
-		});
+		todoListFragment.setListAdapter(aa);
 	}
 
 	@Override
@@ -46,6 +36,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void onNewItemAdded(String newItem){
+		todoItems.add(newItem);
+		aa.notifyDataSetChanged();
 	}
 
 }
---
+++ src/com/example/todolist/ToDoListFragment.java
@@ -0,0 +1,11 @@
+package com.example.todolist;
+
+import android.annotation.TargetApi;
+import android.app.ListFragment;
+import android.os.Build;
+
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class ToDoListFragment extends ListFragment {
+
+}
